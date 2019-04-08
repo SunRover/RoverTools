de2855dd42075621cf156c332cb8d4737bc6314c
vikramkashyap <vkashyap737@gmail.com>
2019-04-08 13:36:42 -0700

    Added ImgWindow and Point classes for use with OpenCV

---
+++ src/tools/Point.java
@@ -0,0 +1,22 @@
+package tools;
+
+public class Point {
+	public double x, y;
+
+	public double x() {
+		return x;
+	}
+
+	public void x(double x) {
+		this.x = x;
+	}
+
+	public double y() {
+		return y;
+	}
+
+	public void y(double y) {
+		this.y = y;
+	}
+
+}
